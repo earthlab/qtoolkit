c79dbbc80b0b98ed62390330d66dde42b5d3ed25
Leah Wasser <leah.wasser@colorado.edu>
2017-11-18 05:27:48 -0700

    linting

--- R/cleanup.R
+++ R/cleanup.R
@@ -7,7 +7,7 @@ split_strings = function(string, n_char = 35) {
   #' a_long_string <- "this is a string that i'd like to wrap"
   #' split_strings(a_long_string, n_char = 15)
 
-  paste(strwrap(string, width=nwrap), collapse="\n")
+  paste(strwrap(string, width = nwrap), collapse = "\n")
 }
 
 split_strings <-  Vectorize(split_strings)
