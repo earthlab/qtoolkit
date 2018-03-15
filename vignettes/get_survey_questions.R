306e9193415366871e4c498c8ec230da27beab7e
Leah Wasser <leah.wasser@colorado.edu>
2018-03-15 17:03:52 -0600

    documumentation updates

---
+++ vignettes/get_survey_questions.R
@@ -0,0 +1,11 @@
+## ----connect, message=FALSE----------------------------------------------
+options(stringsAsFactors = FALSE)
+library(qtoolkit)
+# still working on only importing the pipe element in the package.
+# https://github.com/sckott/analogsea/issues/32
+# note currently i need to just load the pipe and also stringsasfactors as false
+library(dplyr)
+## Connect to Qualtrics API
+qapi_connect()
+
+
