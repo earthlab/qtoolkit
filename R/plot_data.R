7e71681bb44ff5681a0468d8cde1a1233b00e8b0
Leah Wasser <leah.wasser@colorado.edu>
2018-03-14 20:48:21 -0600

    fixing a few more bugs and adding docs

--- R/plot_data.R
+++ R/plot_data.R
@@ -2,25 +2,27 @@
 
 #' get_choices
 #'
-#' Cleaned data.frame all ready for plotting 
+#' Cleaned data.frame all ready for plotting
 #'
 #' @importFrom assertthat assert_that
 #' @importFrom assertthat is.string
 #' @importFrom dplyr mutate
 #' @importFrom dplyr select
 #' @importFrom dplyr rename
+#' @importFrom tidyr gather
+#' @importFrom tidyr separate
 #'
-#' @param choices_df choicse sub object pulled from the qsurvey object.
+#' @param choices_df choices sub object pulled from the qsurvey object.
 #'
-#' @return data.frame of choices ready to be merged with the main question df  
+#' @return data.frame of choices ready to be merged with the main question df
 #' @export
-#' 
+#'
 
 get_choices <- function(choices_df) {
-  choices <- choices_df %>% 
-    select(order, recode, text) %>% 
+  choices <- choices_df %>%
+    select(order, recode, text) %>%
     mutate(text = strip_html(text),
-           recode = as.integer(recode)) %>% 
+           recode = as.integer(recode)) %>%
     rename(qchoice = text)
   return(choices)
 }
@@ -28,7 +30,7 @@ get_choices <- function(choices_df) {
 
 #' get_subq
 #'
-#' Cleaned data.frame all ready for plotting 
+#' Cleaned data.frame all ready for plotting
 #'
 #' @importFrom assertthat assert_that
 #' @importFrom assertthat is.string
@@ -38,15 +40,15 @@ get_choices <- function(choices_df) {
 #'
 #' @param subq_df choicse sub object pulled from the qsurvey object.
 #'
-#' @return data.frame of subquestions ready to be merged with the main question df  
+#' @return data.frame of subquestions ready to be merged with the main question df
 #' @export
-#' 
+#'
 
 
 get_subq <- function(subq_df){
-  fin_subq <- subq_df %>% 
+  fin_subq <- subq_df %>%
     mutate(desc = strip_html(desc),
-           recode = as.integer(recode)) %>% 
+           recode = as.integer(recode)) %>%
     rename(question = text, qdescription = desc)
   return(fin_subq)
 }
@@ -54,7 +56,7 @@ get_subq <- function(subq_df){
 
 #' get_data
 #'
-#' Cleaned data.frame all ready for plotting 
+#' Cleaned data.frame all ready for plotting
 #'
 #' @importFrom assertthat assert_that
 #' @importFrom assertthat is.string
@@ -62,36 +64,36 @@ get_subq <- function(subq_df){
 #' @importFrom dplyr select
 #' @importFrom dplyr starts_with
 #'
-#' @param quest_obj the object within the qsurvey object that contains all relevant information to the question including choicses and subquestions IF those are relevant 
+#' @param quest_obj the object within the qsurvey object that contains all relevant information to the question including choicses and subquestions IF those are relevant
 #'
 #' @return Nice data.frame ready for plotting. yeaaassss
 #' @export
-#' 
+#'
 
 get_question_resp <- function(quest_obj) {
-  
+
   # then gather the data
   # note that "recode is the response value here (i *think*) TODO: look into json structure
-  fin_resp <- quest_obj$responses %>% 
+  fin_resp <- quest_obj$responses %>%
     gather(key = "qnum", value = "response", -ResponseID)
 
   # if the object has choices then join the choices to the question for plotting annalysis
   if (!is.null(quest_obj$choices)){
     # if a question has no choicses i may be able to test for that in the object?? not sure
   choices <- get_choices(quest_obj$choices)
-  
-  fin_resp <- fin_resp %>% 
+
+  fin_resp <- fin_resp %>%
     left_join(choices, by = c("response" = "recode"))
   }
-  
+
   if (!is.null(quest_obj$subquestions)){
-  # if there are subquestions 
+  # if there are subquestions
   subq <- get_subq(quest_obj$subquestions)
-  
+
   # merge subquestions with df for plotting
   fin_resp <- fin_resp %>%
-    separate(qnum, into = c("qnum", "subqnum"), sep = "_") %>% 
-    mutate(subqnum = as.integer(subqnum)) %>% 
+    separate(qnum, into = c("qnum", "subqnum"), sep = "_") %>%
+    mutate(subqnum = as.integer(subqnum)) %>%
     left_join(subq, by = c("subqnum" = "recode"))
   }
   return(fin_resp)
